af987405a53b55ad04f993d3774a6d30966c3b43
Sistemas Paino <88692740+victorca97@users.noreply.github.com>
2022-09-26 11:38:41 -0500

    Front 26/09

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Logo from './componentes/logo.jsx';
+import ListaTareas from './componentes/ListaTareas';
+
+function App() {
+  return (
+    <>
+      <div className="aplicacion-cabecera">
+        <Logo />
+      </div>
+      <form class="nombre-finca-formulario">
+        <h2> Nombre de la Finca: </h2>
+        <input
+          className='tarea-input'
+          type='text'
+          placeholder=''
+          name='texto'
+        />
+      </form>
+      <div className='form-info'>
+        <h2> Servicios públicos </h2>
+        <ListaTareas />
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
