0066f25194ef65610b847eb05fac0025527f29ad
Sistemas Paino <88692740+victorca97@users.noreply.github.com>
2022-10-11 16:01:16 -0500

    11/10/22 v2.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Logo from './componentes/logo.jsx';
 import { Fincas } from './componentes/Fincas';
 import ListaSecciones from './componentes/ListaSecciones';
@@ -6,18 +12,22 @@ import ListaSecciones from './componentes/ListaSecciones';
 function App() {
 
   return (
-    <>
-      <div className="contenedor-cabecera">
-        <Logo />
-      </div>
-      <form className="nombre-finca-formulario">
-        <h2> Nombre de la Finca: </h2>
-        <div className='autocomplete-wrapper'>
-          <Fincas />
-        </div>
-      </form>
-      <ListaSecciones />
-    </>
+    <Router>
+      <Switch>
+        <Route path='/administrador'>
+          <div className="contenedor-cabecera">
+            <Logo />
+          </div>
+          <form className="nombre-finca-formulario">
+            <h2> Nombre de la Finca: </h2>
+            <div className='autocomplete-wrapper'>
+              <Fincas />
+            </div>
+          </form>
+          <ListaSecciones />
+        </Route>
+      </Switch>
+    </Router>
     
   );
 }
