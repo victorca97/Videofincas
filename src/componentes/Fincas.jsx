5a9d25e22a2a5185f273f21ed3517d5b63459d31
JotFe <52864518+JotFe@users.noreply.github.com>
2022-10-20 09:30:45 -0500

    UPDATE WHIT API

--- src/componentes/Fincas.jsx
+++ src/componentes/Fincas.jsx
@@ -20,27 +20,56 @@
 //         </div>
 //     )
 // }
-
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useRef,useState} from 'react';
 import '../estilos/Fincas.css';
 import ReactSelect from 'react-select';
 import axios from 'axios';
 
+function useLocalStorage(itemName,initialValue){
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+    
+    if(!localStorageItem){
+      localStorage.setItem(itemName,JSON.stringify(initialValue));
+      parsedItem=initialValue;
+    }else{
+      parsedItem = JSON.parse(localStorageItem);
+    } 
+
+    const [item,setItem] = React.useState(parsedItem)
+    
+    const saveItem = (newItem) => {
+      const stringfieldItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName,stringfieldItem);
+      setItem(newItem);
+    }
+    return[
+      item,
+      saveItem,
+    ]
+  }
+
 export const Fincas = () => {
+    // ====== CONEXION CON LA API ======
     const [fincas, setFincas] = useState([]);
     useEffect(() => {
-        getPlantilla();
+        getFincas();
     }, []);
 
-    const getPlantilla = async() => {
-        const resp = await axios.get('http://192.168.195.12:4000/propiedades')
-        console.log(resp.data);
+    const getFincas = async() => {
+        const resp = await axios.get('http://127.0.0.1:4000/propiedades')
         setFincas(resp.data);
     };
 
+    const getPlantilla = async(finca_seleccion) => {
+        const resp2 = await axios.get(`http://127.0.0.1:4000/plantilla/${finca_seleccion}`)
+        console.log(resp2.data[0]);
+        localStorage.setItem('plantilla',JSON.stringify(resp2.data[0]));
+    };
+    // ====== CONEXION CON LA API ======
     return (
         <div className='fincas-contenedor'>
-            <ReactSelect
+            <ReactSelect  onChange={(finca_seleccion)=>getPlantilla(finca_seleccion.value)}
                 options = { fincas.map(sup => ({ label: sup._id, value: sup._id})) }
             />
         </div>
