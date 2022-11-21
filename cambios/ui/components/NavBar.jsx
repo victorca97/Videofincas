e28ab08c9fda37fcc38018304269a158994bb92c
Sistemas Paino <88692740+victorca97@users.noreply.github.com>
2022-11-21 11:21:13 -0500

    update y navbar

---
+++ cambios/ui/components/NavBar.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import '../styles-navbar.css'
+import videoFincaLogo from '../../imagenes/logo_videofinca.png'
+import { useLocation, Link } from 'react-router-dom'
+import { AiOutlineLogout } from 'react-icons/ai'
+
+const iconLogout = {
+  width: '50px',
+  height: '50px'
+}
+
+export const NavBar = () => {
+
+  const location = useLocation();
+
+  let name = location.pathname.split('Videofincas/')[1]
+  switch (name) {
+    case 'home':
+      name = 'Menú principal'
+      break;
+    case 'recibo':
+      name = 'Formulario del Recibo';
+      break;
+    case 'propietario':
+      name = 'Formulario del Propietarios'
+      break;
+    case 'propietarios':
+      name = 'Propietarios'
+      break;
+    case 'finca':
+      name = 'Formulario de las Fincas'
+      break;
+    case 'descarga':
+      name = 'Descargas'
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className='contenedor-cabecera'>
+
+      <div className='videofincas-logo-contenedor row'>
+        <Link to='/Videofincas/home' className='col-xs-12 col-sm-4'>
+          <img
+            src={videoFincaLogo}
+            className='videofincas-logo'
+          />
+        </Link>
+
+
+        <div className='titulo-principal col-xs-12 col-sm-6'>
+          <h1>
+            {name}
+          </h1>
+        </div>
+
+        <div className='my-4'>
+          <Link to='/Videofincas' className='main_div col-xs-12 col-sm-2 py-3 '>
+            <button>Salir</button>
+          </Link>
+        </div>
+
+
+      </div>
+    </div>
+
+  )
+}
